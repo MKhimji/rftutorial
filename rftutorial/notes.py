bdc23dd0ba2016349c5d4aa5700f8116193a3919
MKhimji <muhammadkhimji@live.co.uk>
2019-06-28 02:10:47 +0000

    install req

---
+++ rftutorial/notes.py
@@ -0,0 +1,7 @@
+# 280619
+
+# Rest framework:  you can expose your data to others so they
+# can make a request for data from your webapp
+# OR
+# you can have a setup so that you request data from other web apps
+# and modify/represent the data how you want.
